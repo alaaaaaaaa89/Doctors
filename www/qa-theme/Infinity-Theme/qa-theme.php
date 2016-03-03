be8f9206243dbc52fd71b717a1d39fd97707a88b
alaaaaaaaa89 <alaaaes@gmail.com>
2016-03-03 10:53:17 +0200

    init commit

---
+++ www/qa-theme/Infinity-Theme/qa-theme.php
@@ -0,0 +1,38 @@
+<?php
+/*
+	don't allow this page to be requested directly from browser 
+*/
+	if (!defined('QA_VERSION')) {
+			header('Location: /');
+			exit;
+	}
+/*
+	Definitions
+*/	
+	define('MYTHEME_DIR', dirname( __FILE__ ));
+	define('MYTHEME_URL',  qa_opt('site_url') . 'qa-theme/' . qa_get_site_theme() . '/');
+	
+	// set layout cookies
+	$layout = qa_opt('it_layout_lists');
+	if($layout)
+		setcookie('layoutdefault', $layout, time()+86400*3650, '/', QA_COOKIE_DOMAIN);
+	else
+		setcookie('layoutdefault', 'masonry', time()+86400*3650, '/', QA_COOKIE_DOMAIN);
+
+	require MYTHEME_DIR. '/functions.php';		
+	require MYTHEME_DIR. '/qa-layer-base.php';		
+	
+	if(isset($_REQUEST['qat_ajax_req'])){
+		qa_register_layer('/qa-layer-ajax.php', 'QAT Ajax Theme Layer', MYTHEME_DIR , MYTHEME_URL );
+		die();
+	}
+
+	
+public function body_script() { 
+    parent::body_script(); 
+    $this->output('<script>CKEDITOR.env.isCompatible = true;</script>'); 
+}
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
